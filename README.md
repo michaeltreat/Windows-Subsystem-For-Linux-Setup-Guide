ee370cf00cbc1f0d491dcd8cdfaf03570242a30d
Michael Treat <michael.leon.treat@gmail.com>
2019-02-06 17:59:43 -0800

    added app.js to manage local storage and UX features to checklist.

--- README.md
+++ README.md
@@ -36,19 +36,7 @@ By the end of this guide, you will know:
 
 ### Checklist:
 
-This checklist can be used to help you keep track of all necessary steps that you need to complete in order to 'Complete' the WSL setup. 
-
-Required:
-- [ ] Enable WSL Feature through Windows.
-- [ ] Download Ubuntu from the Windows Store.
-- [ ] Setup Ubuntu Username and password.
-
-Optional:
-- [ ] Update your terminal profile.
-- [ ] Set working aliases to help with navigation.
-- [ ] Create a top-level NPM package.json to hold Windows-Side global packages. ( If you plan on using NPM at all that is.)
-- [ ] Review recommended work-flow and software installation notes.
-- [ ] Star this page and subsribe to my channel on YouTube :D Sorry, had to do it! 
+This link has a checklist that allows you to track your progess : ( link will be added later.)
 
 ### Upcoming Additions:
 
--- src/app.js
+++ src/app.js
@@ -1,14 +1,57 @@
 'use strict';
 
+/*
+    This has 2 primary functions:
+        1. Allow the user to click on either a check box, or the 
+            label ( the words ) for that check box
+        
+        2. Manages local storage. Some what hacky implementation, but reduces a 
+            for loop cycle.
+*/
+
+
+//Setup LocalStorage Caching to store progress.
+var state = {};
+var fLocalStorage = false;
 var labels = document.getElementsByTagName('label');
 
-// Enables the user to click on the words or the box in order to check it.
-for( let i = 0; i < labels.length; i++){
-    labels[i].addEventListener('click', e => {
-        // Grab the input tag off the target.
-        let checkbox = e.target.children[0];
+// Changes the state object, and updates the value in localStorage.
+function changeState(label, status){
+    state[label] = status;
+    localStorage.state = JSON.stringify(state)
+};
 
-        //Toggle check box when you click on any part of the .
-        checkbox.checked = !checkbox.checked;
+
+// If there is a local storage, reassign the previous state to the checkboxes.
+if(localStorage.state){
+    state = JSON.parse(localStorage.state);
+    fLocalStorage = true;
+    
+    let boxes = document.getElementsByTagName('input')
+    
+    // When assiging each box, use the boxes name as the key
+    // to retrieve the value from the state object
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].checked = state[boxes[i].name];    
+    }
+} 
+
+// Sets handler to enable the user to click on the words or the box in order to check it. 
+// Also initializes the first state if it hasn't already been sent to local storage.
+for( let i = 0; i < labels.length; i++){
+
+    // If no local storage yet, initialize a state object with keys matching the label names.
+    if(!fLocalStorage) state[labels[i].htmlFor] = false;
+
+    labels[i].addEventListener('click', e => {
+        if(e.target.tagName === 'LABEL'){
+            
+            // Grab the input tag off the target.
+            let checkbox = e.target.children[0];
+            
+            //Toggle check box when you click on any part of the .
+            checkbox.checked = !checkbox.checked;
+            changeState(labels[i].htmlFor, checkbox.checked)
+        }
     });
-}
+};
